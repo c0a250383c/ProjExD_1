ce8ba018f21c0dd0f55320514dccd76e88be628d
yuuuuuki <c0a250383@edu.teu.ac.jp>
2026-04-14 16:46:15 +0900

    演習3：プログラム名を flying_kokaton.py に変更

---
+++ flying_kokaton.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")  # 練習1
+    bg_img2 = pg.transform.flip(bg_img, True, False)  # 練習8
+    kk_img = pg.image.load("fig/3.png")  # 練習3
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()  # 練習10：こうかとんRectの取得
+    kk_rct.center = 300, 200  # 練習10：こうかとんの初期座標の設定
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        
+        sum_mv = [-1, 0] 
+
+        key_lst = pg.key.get_pressed()
+        
+        if key_lst[pg.K_UP]:    # 上：y軸マイナス
+            sum_mv[1] -= 1
+        if key_lst[pg.K_DOWN]:  # 下：y軸プラス
+            sum_mv[1] += 1
+        if key_lst[pg.K_LEFT]:  # 左：x軸をさらにマイナス
+            sum_mv[0] -= 1
+        if key_lst[pg.K_RIGHT]: # 右：x軸に+2加算
+            sum_mv[0] += 2      # (-1 + 2 = +1) なので、右に自力で進める
+
+        # 2. move_ip はここで「1回だけ」呼び出す
+        kk_rct.move_ip(sum_mv)
+
+        # 以降、描画処理
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(kk_img, kk_rct)
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
